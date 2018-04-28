6cdd014dc6ec7b19f8cf41b1df1d0e461887436f
te-sh <admire.japan@gmail.com>
2018-04-29 00:13:21 +0900

    upgrade libraries

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.listen(port);
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/../dist/index.html'));
+  res.sendFile(path.join(__dirname + '/../dist/worldwolf/index.html'));
 });
 
 console.log(`Server listening on ${port}`);
