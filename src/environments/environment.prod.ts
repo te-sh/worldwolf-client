0e2704b244ada76d5c038d6ee3e4edd61b50bfaa
te-sh <admire.japan@gmail.com>
2018-04-29 11:21:05 +0900

    use secure websocket

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   api: {
     http: 'https://worldwolf-api.herokuapp.com',
-    websocket: 'ws://worldwolf-api.herokuapp.com'
+    websocket: 'wss://worldwolf-api.herokuapp.com'
   }
 };
